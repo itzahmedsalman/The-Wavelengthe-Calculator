f85ae719d11fb3e83f3404069d7ac0d6ff6b5efc
Salman Ahmed <itzahmedsalman@gmail.com>
2025-07-19 00:04:16 +0600

    Problem 1.c

--- Problem 1.c
+++ Problem 1.c
@@ -2,10 +2,12 @@
 #include<math.h>
 int main()
 {
+    //Assigning variables
     int m;
-
     double d, theta, wave_length;
 
+
+    //Collecting input data
     printf("Enter the difference between the slits(d) in micrometer(10^-6): ");
     scanf("%lf", &d);
 
@@ -15,12 +17,20 @@ int main()
     printf("Enter the order of maximum (m): ");
     scanf("%d", &m);
 
+
+    //Conversion of angle from degree to radian
     double radian = theta * (M_PI/180.0);
 
+
+    //Calculating the wavelength
     wave_length = (d * 0.00001 * sin(radian))/ (m * 0.00000001);
 
-    printf("The numeric value of the wavelength is %lf\n", wave_length);
 
+    //Printing the numeric value of the wavelength
+    printf("\nThe numeric value of the wavelength is %lf Nanometer\n", wave_length);
+
+
+    //Defining the colour of the light based on the wavelenth
 
 
     return 0;
